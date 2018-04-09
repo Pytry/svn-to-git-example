cc9318cd9e989247a213c4b8b22759738c144306
Bob Bob <bob@bob.bob>
2018-04-09 00:48:05 +0000

    Added comment for target git repo
    git-svn-id: file:///home/bob/vcs-repos/svn-repos/svn-to-git-example/trunk@5 68229183-97c8-4ec0-8540-fd282a2975b8

--- git-svn-clone.sh
+++ git-svn-clone.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 git svn clone \
-    svn://localhost/home/bob/vcs-repos/svn-repo/svn-to-git-example \
-    svn-to-git-example \
-    --authors-file=/home/bob/Development/authors.txt
+    svn://localhost/home/bob/vcs-repos/svn-repos/svn-to-git-example \
+    --authors-file=/home/bob/Development/svn-to-git-example/trunk/authors.txt
 
-git svn clone svn://localhost/home/bob/vcs-repos/svn-repos/svn-to-git-example --authors-file=/home/bob/Development/authors.txt
+# https://github.com/Pytry/svn-to-git-example.git
