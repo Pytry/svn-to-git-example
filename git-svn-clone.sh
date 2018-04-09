7e87fa2ff238054c5b9912a697a5c5b184f84a91
Bob Bob <bob@bob.bob>
2018-04-09 03:29:11 +0000

    Updated to checkout trunk
    git-svn-id: file:///home/bob/vcs-repos/svn-repos/svn-to-git-example/trunk@11 68229183-97c8-4ec0-8540-fd282a2975b8

--- git-svn-clone.sh
+++ git-svn-clone.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 git svn clone \
-    svn://localhost/home/bob/vcs-repos/svn-repos/svn-to-git-example \
+    svn://localhost/home/bob/vcs-repos/svn-repos/svn-to-git-example/trunk \
+    svn-to-git-example \
     --authors-file=/home/bob/Development/svn-to-git-example/trunk/authors.txt
 
 # https://github.com/Pytry/svn-to-git-example.git
--- tags-to-git.sh
+++ tags-to-git.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-for t in $(git for-each-ref --format='%(refname:short)' refs/remotes/tags);
-do
-    git tag ${t/tags\//} $t && git branch -D -r $t;
-done
+for t in $(git for-each-ref --format='%(refname:short)' refs/remotes/tags); do git tag ${t/tags\//} $t && git branch -D -r $t; done
