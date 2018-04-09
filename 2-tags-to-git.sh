804acce59b5777e638e1797edc9f26c47df5a682
Bob Bob <bob@bob.bob>
2018-04-09 05:35:53 +0000

    Fixed tag converter to not include the "origin" prefix
    git-svn-id: file:///home/bob/vcs-repos/svn-repos/svn-to-git-example/trunk@19 68229183-97c8-4ec0-8540-fd282a2975b8

--- 2-tags-to-git.sh
+++ 2-tags-to-git.sh
@@ -2,8 +2,8 @@
 # This should only be run in the newly created/converted GIT repo, and not in the checked out SVN repo
 for t in $(git for-each-ref --format='%(refname:short)' refs/remotes/origin/tags)
 do
-    echo "Converting tag '"${t/tags\//}"'"
-    git tag ${t/origin/tags\//} $t
+    echo "Converting tag '"${t/origin\/tags\//}"'"
+    git tag ${t/origin\/tags\//} $t
     git branch -D -r $t
 done
 
