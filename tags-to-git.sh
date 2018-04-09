f1fc18eb43235e2f64080ad778a0ee02a30196e3
Bob Bob <bob@bob.bob>
2018-04-09 04:11:39 +0000

    Fix tag conversion script
    git-svn-id: file:///home/bob/vcs-repos/svn-repos/svn-to-git-example/trunk@13 68229183-97c8-4ec0-8540-fd282a2975b8

--- tags-to-git.sh
+++ tags-to-git.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-for t in $(git for-each-ref --format='%(refname:short)' refs/remotes/tags); do git tag ${t/tags\//} $t && git branch -D -r $t; done
+for t in $(git for-each-ref --format='%(refname:short)' refs/remotes/origin/tags)
+do
+    echo $t
+    echo ${t/tags\//}
+    git tag ${t/tags\//} $t
+    git branch -D -r $t
+done
